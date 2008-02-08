569a237316b005af5ca6a3053bde080c7b610137
Bryan Tong Minh <bryan.tongminh@gmail.com>
2008-02-08 14:49:00 +0000

    Revert timestamp stuff

--- client.py
+++ client.py
@@ -23,8 +23,8 @@ except ImportError:
 
 def parse_timestamp(t):
 	if t == '0000-00-00T00:00:00Z':
-		return (0, 0, 0, 0, 0, 0)
-	return time.strptime(t, '%Y-%m-%dT%H:%M:%SZ')[:6]
+		return (0, 0, 0, 0, 0, 0, 0, 0)
+	return time.strptime(t, '%Y-%m-%dT%H:%M:%SZ')
 	
 class WaitToken(object):
 	def __init__(self):
