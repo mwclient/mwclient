dfb5cbc8eed2889cf3cccd46f5d554375de7b9de
Dan Michael O. Heggø <danmichaelo@gmail.com>
2013-06-16 22:25:46 +0200

    add support for Travis continuous integration service

---
+++ tests/client-test.py
@@ -0,0 +1,23 @@
+#encoding=utf-8
+from __future__ import unicode_literals
+import unittest
+import sys
+import os
+myPath = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, myPath + '/../')
+
+from mwclient import Site
+
+
+class TestClient(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_setup(self):
+        # Check that templates can be found
+        site = Site('commons.wikimedia.org')
+        self.assertTrue(site.initialized)
+
+if __name__ == '__main__':
+    unittest.main()
