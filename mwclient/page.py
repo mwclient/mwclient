mwclient/mwclient

commit a8709c0e2126d41a83c880e77b5d63023ddd5184

Remove compability methods for MediaWiki < 1.16
Since we now require MediaWiki 1.16, these are no longer needed. It
might be necessary to add new compability methods in the future, but the
API is generally stable now.

mwclient/page.py
@@ -1,7 +1,6 @@
 import client
 import errors
 import listing
-import compatibility
 from page_nowriteapi import OldPage
 
 import urllib
@@ -97,7 +96,7 @@ class Page(object):
     def can(self, action):
         level = self.protection.get(action, (action, ))[0]
         if level == 'sysop':
-            level = compatibility.protectright(self.site.version)
+            level = 'editprotected'
 
         return level in self.site.rights
 
@@ -293,7 +292,7 @@ class Page(object):
                                                    namespace=namespace, filterredir=filterredir))
         if redirect:
             kwargs['%sredirect' % prefix] = '1'
-        kwargs[compatibility.title(prefix, self.site.require(1, 11, raise_error=False))] = self.name
+        kwargs[prefix + 'title'] = self.name
 
         return listing.List.get_list(generator)(self.site, 'backlinks', 'bl', limit=limit, return_values='title', **kwargs)
 
@@ -313,7 +312,7 @@ class Page(object):
                                                    namespace=namespace, filterredir=filterredir))
         if redirect:
             kwargs['%sredirect' % prefix] = '1'
-        kwargs[compatibility.title(prefix, self.site.require(1, 11, raise_error=False))] = self.name
+        kwargs[prefix + 'title'] = self.name
 
         return listing.List.get_list(generator)(self.site, 'embeddedin', 'ei', limit=limit, return_values='title', **kwargs)
 
@@ -375,14 +374,15 @@ class Image(Page):
     def __init__(self, site, name, info=None):
         site.require(1, 11)
         Page.__init__(self, site, name, info,
-                      extra_properties={'imageinfo': (('iiprop',
-                                                       compatibility.iiprop(site.version)), )})
+                      extra_properties={'imageinfo':
+                                        (('iiprop', 'timestamp|user|comment|url|size|sha1|metadata|archivename'))
+                                        })
         self.imagerepository = self._info.get('imagerepository', '')
         self.imageinfo = self._info.get('imageinfo', ({}, ))[0]
 
     def imagehistory(self):
         return listing.PageProperty(self, 'imageinfo', 'ii',
-                                    iiprop=compatibility.iiprop(self.site.version))
+                                    iiprop='timestamp|user|comment|url|size|sha1|metadata|archivename')
 
     def imageusage(self, namespace=None, filterredir='all', redirect=False,
                    limit=None, generator=True):

mwclient/client.py
@@ -24,7 +24,6 @@ import requests
 import errors
 import listing
 import page
-import compatibility
 
 try:
     import gzip
@@ -143,11 +142,12 @@ class Site(object):
                 raise errors.MediaWikiVersionError('Unknown MediaWiki %s' % '.'.join(version))
         else:
             raise errors.MediaWikiVersionError('Unknown generator %s' % self.site['generator'])
-        # Require 1.11 until some compatibility issues are fixed
-        self.require(1, 11)
+
+        # Require 1.16
+        self.require(1, 16)
 
         # User info
-        userinfo = compatibility.userinfo(meta, self.require(1, 12, raise_error=False))
+        userinfo = meta['query']['userinfo']
         self.username = userinfo['name']
         self.groups = userinfo.get('groups', [])
         self.rights = userinfo.get('rights', [])
@@ -187,7 +187,7 @@ class Site(object):
             token = self.wait_token()
 
         try:
-            userinfo = compatibility.userinfo(info, self.require(1, 12, raise_error=None))
+            userinfo = info['query']['userinfo']
         except KeyError:
             userinfo = ()
         if 'blockedby' in userinfo:
@@ -361,7 +361,7 @@ class Site(object):
 
         if self.initialized:
             info = self.api('query', meta='userinfo', uiprop='groups|rights')
-            userinfo = compatibility.userinfo(info, self.require(1, 12, raise_error=False))
+            userinfo = info['query']['userinfo']
             self.username = userinfo['name']
             self.groups = userinfo.get('groups', [])
             self.rights = userinfo.get('rights', [])
@@ -396,10 +396,6 @@ class Site(object):
         >>> client.upload(open('somefile', 'rb'), filename='somefile.jpg',
                           description='Some description')
         """
-        if self.version[:2] < (1, 16):
-            return compatibility.old_upload(self, file=file, filename=filename,
-                                            description=description, ignore=ignore,
-                                            file_size=file_size)
 
         if file_size is not None:
             # Note that DeprecationWarning is hidden by default since Python 2.7

mwclient/compatibility.py
@@ -1,52 +1,0 @@
-import errors
-
-
-def title(prefix, new_format):
-    if new_format:
-        return prefix + 'title'
-    else:
-        return 'titles'
-
-
-def userinfo(data, new_format=None):
-    if new_format is None:
-        # Unknown version; trying to guess
-        if 'userinfo' in data:
-            return data['userinfo']
-        elif 'userinfo' in data.get('query', ()):
-            return data['query']['userinfo']
-        else:
-            return {}
-    elif new_format:
-        return data['query']['userinfo']
-    else:
-        return data['userinfo']
-
-
-def iiprop(version):
-    if version[:2] >= (1, 13):
-        return 'timestamp|user|comment|url|size|sha1|metadata|archivename'
-    if version[:2] >= (1, 12):
-        return 'timestamp|user|comment|url|size|sha1|metadata'
-    else:
-        return 'timestamp|user|comment|url|size|sha1'
-
-
-def cmtitle(page, new_format, prefix=''):
-    if new_format:
-        return prefix + 'title', page.name
-    else:
-        return prefix + 'category', page.strip_namespace(page.name)
-
-
-def protectright(version):
-    if version[:2] >= (1, 13):
-        return 'editprotected'
-    else:
-        return 'protect'
-
-from cStringIO import StringIO
-
-
-def old_upload(self, file, filename, description, license='', ignore=False, file_size=None):
-    raise MwClientError('The old_upload method has been removed in version 0.7 of MwClient')

mwclient/listing.py
@@ -1,6 +1,5 @@
 import client
 import page
-import compatibility
 
 
 class List(object):
@@ -128,7 +127,7 @@ class GeneratorList(List):
     def load_chunk(self):
         # Put this here so that the constructor does not fail
         # on uninitialized sites
-        self.args['iiprop'] = compatibility.iiprop(self.site.version)
+        self.args['iiprop'] = 'timestamp|user|comment|url|size|sha1|metadata|archivename'
         return List.load_chunk(self)
 
 
@@ -137,8 +136,7 @@ class Category(page.Page, GeneratorList):
     def __init__(self, site, name, info=None, namespace=None):
         page.Page.__init__(self, site, name, info)
         kwargs = {}
-        kwargs.update((compatibility.cmtitle(self, self.site.require(
-            1, 12, raise_error=False), prefix='gcm'), ))
+        kwargs['gcmtitle'] = self.name
         if namespace:
             kwargs['gcmnamespace'] = namespace
         GeneratorList.__init__(self, site, 'categorymembers', 'cm', **kwargs)
@@ -150,8 +148,7 @@ class Category(page.Page, GeneratorList):
                 dir='asc', start=None, end=None, generator=True):
         prefix = self.get_prefix('cm', generator)
         kwargs = dict(self.generate_kwargs(prefix, prop=prop, namespace=namespace,
-                                           sort=sort, dir=dir, start=start, end=end, *(compatibility.cmtitle(
-                                                                                       self, self.site.require(1, 12, raise_error=False)), )))
+                                           sort=sort, dir=dir, start=start, end=end, title=self.name))
         return self.get_list(generator)(self.site, 'categorymembers', 'cm', **kwargs)
 
 

tests/test_client.py
@@ -94,7 +94,17 @@ class TestClient(unittest.TestCase):
         assert 'meta=siteinfo%7Cuserinfo' in responses.calls[0].request.body
 
     @responses.activate
+    def test_api_disabled(self):
+        # Should raise APIDisabledError if API is not enabled
+
+        self.httpShouldReturn('MediaWiki API is not enabled for this site.')
+
+        with pytest.raises(mwclient.errors.APIDisabledError):
+            site = mwclient.Site('test.wikipedia.org')
+
+    @responses.activate
     def test_version(self):
+        # Should parse the MediaWiki version number correctly
 
         self.httpShouldReturn(self.makeMetaResponse(version='1.16'))
 
@@ -104,17 +114,10 @@ class TestClient(unittest.TestCase):
         assert site.version == (1, 16)
 
     @responses.activate
-    def test_api_disabled(self):
-
-        self.httpShouldReturn('MediaWiki API is not enabled for this site.')
-
-        with pytest.raises(mwclient.errors.APIDisabledError):
-            site = mwclient.Site('test.wikipedia.org')
-
-    @responses.activate
     def test_min_version(self):
+        # Should raise MediaWikiVersionError if API version is < 1.16
 
-        self.httpShouldReturn(self.makeMetaResponse(version='1.10'))
+        self.httpShouldReturn(self.makeMetaResponse(version='1.15'))
 
         with pytest.raises(mwclient.errors.MediaWikiVersionError):
             site = mwclient.Site('test.wikipedia.org')
@@ -129,6 +132,7 @@ class TestClient(unittest.TestCase):
 
     @responses.activate
     def test_raw_index(self):
+        # Initializing the client should result in one request
 
         site = self.stdSetup()
 

commit 71e3a05a1ce41c88745d0c089ad29bde4b21b492

Fix bug introduced in a8709c0
The missing comma would cause site.images[filename] to fail

mwclient/page.py
@@ -382,7 +382,7 @@ class Image(Page):
         site.require(1, 11)
         Page.__init__(self, site, name, info,
                       extra_properties={'imageinfo':
-                                        (('iiprop', 'timestamp|user|comment|url|size|sha1|metadata|archivename'))
+                                        (('iiprop', 'timestamp|user|comment|url|size|sha1|metadata|archivename'), )
                                         })
         self.imagerepository = self._info.get('imagerepository', '')
         self.imageinfo = self._info.get('imageinfo', ({}, ))[0]