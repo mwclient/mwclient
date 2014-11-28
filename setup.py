c9ea5e6d3227a267c11c624f2f09dfbcb0a17ea1
Luis Miguel Morillas <morillas@gmail.com>
2014-11-28 09:13:26 +0100

    Adds six as requirement. It solves "ImportError: No module named six"

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ class PyTest(TestCommand):
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()
 
-requirements = ['requests']
+requirements = ['requests', 'six']
 if sys.version_info < (2, 7):
     requirements.append('ordereddict')
 
