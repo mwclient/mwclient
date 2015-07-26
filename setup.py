22ac6ecb8936963218fb7240c7c3c04322ca8a2a
Dan Michael O. Heggø <danmichaelo@gmail.com>
2015-07-26 22:08:35 +0200

    [#52] Add Python 3.3 and 3.4 to setup.py

--- setup.py
+++ setup.py
@@ -45,7 +45,9 @@ setup(name='mwclient',
       classifiers=[
           'Programming Language :: Python',
           'Programming Language :: Python :: 2.6',
-          'Programming Language :: Python :: 2.7'
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4'
       ],
       keywords='mediawiki wikipedia',
       author='Bryan Tong Minh',
