7d4c0fc302931f97c6c450b2b41cc48dd499ec84
Dan Michael O. Heggø <danmichaelo@gmail.com>
2017-07-31 02:35:29 +0200

    Simplify test setup

--- setup.py
+++ setup.py
@@ -3,40 +3,10 @@
 from __future__ import print_function
 import os
 import sys
-
-try:
-    from setuptools import setup
-    from setuptools.command.test import test as TestCommand
-except ImportError:
-    print("This package requires 'setuptools' to be installed.")
-    sys.exit(1)
-
-
-class PyTest(TestCommand):
-    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
-
-    def initialize_options(self):
-        TestCommand.initialize_options(self)
-        self.pytest_args = '-v --pep8 tests mwclient --cov mwclient'
-
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-
-    def run_tests(self):
-        # Import here, cause outside the eggs aren't loaded
-        import pytest
-        errno = pytest.main(self.pytest_args)
-        sys.exit(errno)
-
+from setuptools import setup
 
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()
-
-requirements = ['requests_oauthlib', 'six']
-if sys.version_info < (2, 7):
-    requirements.append('ordereddict')
 
 setup(name='mwclient',
       version='0.8.5',  # Use bumpversion to update
@@ -47,7 +17,8 @@ setup(name='mwclient',
           'Programming Language :: Python :: 2.7',
           'Programming Language :: Python :: 3.3',
           'Programming Language :: Python :: 3.4',
-          'Programming Language :: Python :: 3.5'
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
       ],
       keywords='mediawiki wikipedia',
       author='Bryan Tong Minh',
@@ -55,8 +26,9 @@ setup(name='mwclient',
       url='https://github.com/btongminh/mwclient',
       license='MIT',
       packages=['mwclient'],
-      cmdclass={'test': PyTest},
-      tests_require=['pytest-pep8', 'pytest-cache', 'pytest', 'pytest-cov', 'responses>=0.3.0', 'mock'],
-      install_requires=requirements,
+      install_requires=['requests_oauthlib', 'six'],
+      setup_requires=['pytest-runner'],
+      tests_require=['pytest', 'pytest-pep8', 'pytest-cache', 'pytest-cov',
+                     'responses>=0.3.0', 'mock'],
       zip_safe=True
       )
