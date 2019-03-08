ffeb37c4a25c4fbb0307e55cb747602f1963f309
Marc Trölitzsch <Marc.Troelitzsch@gmail.com>
2019-03-08 20:19:18 +0100

    Adding documentation to parse_timestamp() while I'm at it

--- mwclient/util.py
+++ mwclient/util.py
@@ -3,6 +3,14 @@ import io
 
 
 def parse_timestamp(t):
+    """Parses a string containing a timestamp.
+
+    Args:
+        t (str): A string containing a timestamp.
+
+    Returns:
+        time.struct_time: A timestamp.
+    """
     if t is None or t == '0000-00-00T00:00:00Z':
         return time.struct_time((0, 0, 0, 0, 0, 0, 0, 0, 0))
     return time.strptime(t, '%Y-%m-%dT%H:%M:%SZ')
