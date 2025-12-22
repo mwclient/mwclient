mwclient/mwclient

commit c23abae3b95203952c9971f143f3a162dd5d2960

Make the Site.revisions() result easier to consume
- Removing the need to use a double for loop to consume the the result
  by returning a list of revisions rather than a list of pages.
- Parsing the timestamps using the standard parse_timestamp method
- Adding tests

(#84)

mwclient/client.py
@@ -21,6 +21,7 @@ from requests.auth import HTTPBasicAuth, AuthBase
 import mwclient.errors as errors
 import mwclient.listing as listing
 from mwclient.sleep import Sleepers
+from mwclient.util import parse_timestamp
 
 try:
     import gzip
@@ -699,9 +700,8 @@ class Site(object):
 
         Example: Get revision text for two revisions:
 
-            >>> for page in site.revisions([689697696, 689816909], prop='content').values():
-            ...     for rev in page['revisions']:
-            ...         print rev['*']
+            >>> for revision in site.revisions([689697696, 689816909], prop='content'):
+            ...     print revision['*']
 
         Args:
             revids (list): A list of (max 50) revisions.
@@ -712,7 +712,7 @@ class Site(object):
                           revision respectively.
 
         Returns:
-            API response as a dictionary.
+            A list of revisions
         """
         kwargs = {
             'prop': 'revisions',
@@ -724,7 +724,15 @@ class Site(object):
         if diffto:
             kwargs['rvdiffto'] = diffto
 
-        return self.api('query', **kwargs).get('query', {}).get('pages')
+        revisions = []
+        pages = self.api('query', **kwargs).get('query', {}).get('pages', {}).values()
+        for page in pages:
+            for revision in page.get('revisions', ()):
+                revision['pageid'] = page.get('pageid')
+                revision['pagetitle'] = page.get('title')
+                revision['timestamp'] = parse_timestamp(revision['timestamp'])
+                revisions.append(revision)
+        return revisions
 
     def search(self, search, namespace='0', what=None, redirects=False, limit=None):
         """

tests/test_client.py
@@ -10,6 +10,7 @@ import requests
 import responses
 import pkg_resources  # part of setuptools
 import mock
+import time
 
 try:
     import json
@@ -27,7 +28,7 @@ logging.basicConfig(level=logging.DEBUG)
 
 class TestCase(unittest.TestCase):
 
-    def makeMetaResponse(self, **kwargs):
+    def metaResponse(self, **kwargs):
         tpl = '{"query":{"general":{"generator":"MediaWiki %(version)s"},"namespaces":{"-1":{"*":"Special","canonical":"Special","case":"first-letter","id":-1},"-2":{"*":"Media","canonical":"Media","case":"first-letter","id":-2},"0":{"*":"","case":"first-letter","content":"","id":0},"1":{"*":"Talk","canonical":"Talk","case":"first-letter","id":1,"subpages":""},"10":{"*":"Template","canonical":"Template","case":"first-letter","id":10,"subpages":""},"100":{"*":"Test namespace 1","canonical":"Test namespace 1","case":"first-letter","id":100,"subpages":""},"101":{"*":"Test namespace 1 talk","canonical":"Test namespace 1 talk","case":"first-letter","id":101,"subpages":""},"102":{"*":"Test namespace 2","canonical":"Test namespace 2","case":"first-letter","id":102,"subpages":""},"103":{"*":"Test namespace 2 talk","canonical":"Test namespace 2 talk","case":"first-letter","id":103,"subpages":""},"11":{"*":"Template talk","canonical":"Template talk","case":"first-letter","id":11,"subpages":""},"1198":{"*":"Translations","canonical":"Translations","case":"first-letter","id":1198,"subpages":""},"1199":{"*":"Translations talk","canonical":"Translations talk","case":"first-letter","id":1199,"subpages":""},"12":{"*":"Help","canonical":"Help","case":"first-letter","id":12,"subpages":""},"13":{"*":"Help talk","canonical":"Help talk","case":"first-letter","id":13,"subpages":""},"14":{"*":"Category","canonical":"Category","case":"first-letter","id":14},"15":{"*":"Category talk","canonical":"Category talk","case":"first-letter","id":15,"subpages":""},"2":{"*":"User","canonical":"User","case":"first-letter","id":2,"subpages":""},"2500":{"*":"VisualEditor","canonical":"VisualEditor","case":"first-letter","id":2500},"2501":{"*":"VisualEditor talk","canonical":"VisualEditor talk","case":"first-letter","id":2501},"2600":{"*":"Topic","canonical":"Topic","case":"first-letter","defaultcontentmodel":"flow-board","id":2600},"3":{"*":"User talk","canonical":"User talk","case":"first-letter","id":3,"subpages":""},"4":{"*":"Wikipedia","canonical":"Project","case":"first-letter","id":4,"subpages":""},"460":{"*":"Campaign","canonical":"Campaign","case":"case-sensitive","defaultcontentmodel":"Campaign","id":460},"461":{"*":"Campaign talk","canonical":"Campaign talk","case":"case-sensitive","id":461},"5":{"*":"Wikipedia talk","canonical":"Project talk","case":"first-letter","id":5,"subpages":""},"6":{"*":"File","canonical":"File","case":"first-letter","id":6},"7":{"*":"File talk","canonical":"File talk","case":"first-letter","id":7,"subpages":""},"710":{"*":"TimedText","canonical":"TimedText","case":"first-letter","id":710},"711":{"*":"TimedText talk","canonical":"TimedText talk","case":"first-letter","id":711},"8":{"*":"MediaWiki","canonical":"MediaWiki","case":"first-letter","id":8,"subpages":""},"828":{"*":"Module","canonical":"Module","case":"first-letter","id":828,"subpages":""},"829":{"*":"Module talk","canonical":"Module talk","case":"first-letter","id":829,"subpages":""},"866":{"*":"CNBanner","canonical":"CNBanner","case":"first-letter","id":866},"867":{"*":"CNBanner talk","canonical":"CNBanner talk","case":"first-letter","id":867,"subpages":""},"9":{"*":"MediaWiki talk","canonical":"MediaWiki talk","case":"first-letter","id":9,"subpages":""},"90":{"*":"Thread","canonical":"Thread","case":"first-letter","id":90},"91":{"*":"Thread talk","canonical":"Thread talk","case":"first-letter","id":91},"92":{"*":"Summary","canonical":"Summary","case":"first-letter","id":92},"93":{"*":"Summary talk","canonical":"Summary talk","case":"first-letter","id":93}},"userinfo":{"anon":"","groups":["*"],"id":0,"name":"127.0.0.1","rights": %(rights)s}}}'
         tpl = tpl % {'version': kwargs.get('version', '1.24wmf17'),
                      'rights': json.dumps(kwargs.get('rights', ["createaccount", "read", "edit", "createpage", "createtalk", "writeapi", "editmyusercss", "editmyuserjs", "viewmywatchlist", "editmywatchlist", "viewmyprivateinfo", "editmyprivateinfo", "editmyoptions", "centralauth-merge", "abusefilter-view", "abusefilter-log", "translate", "vipsscaler-test", "upload"]))
@@ -37,7 +38,10 @@ class TestCase(unittest.TestCase):
         if kwargs.get('writeapi', True):
             res['query']['general']['writeapi'] = ''
 
-        return json.dumps(res)
+        return res
+
+    def metaResponseAsJson(self, **kwargs):
+        return json.dumps(self.metaResponse(**kwargs))
 
     def httpShouldReturn(self, body=None, callback=None, scheme='http', host='test.wikipedia.org', path='/w/',
                          script='api', headers=None, status=200):
@@ -49,7 +53,7 @@ class TestCase(unittest.TestCase):
                           adding_headers=headers, status=status)
 
     def stdSetup(self):
-        self.httpShouldReturn(self.makeMetaResponse())
+        self.httpShouldReturn(self.metaResponseAsJson())
         site = mwclient.Site('test.wikipedia.org')
         responses.reset()
         return site
@@ -95,7 +99,7 @@ class TestClient(TestCase):
     def test_http_as_default(self):
         # 'http' should be the default scheme (for historical reasons)
 
-        self.httpShouldReturn(self.makeMetaResponse(), scheme='http')
+        self.httpShouldReturn(self.metaResponseAsJson(), scheme='http')
 
         site = mwclient.Site('test.wikipedia.org')
 
@@ -110,7 +114,7 @@ class TestClient(TestCase):
             if len(responses.calls) == 0:
                 return (200, {'x-database-lag': '0', 'retry-after': '0'}, '')
             else:
-                return (200, {}, self.makeMetaResponse())
+                return (200, {}, self.metaResponseAsJson())
 
         self.httpShouldReturn(callback=request_callback, scheme='http')
 
@@ -133,7 +137,7 @@ class TestClient(TestCase):
     def test_headers(self):
         # Content-type should be 'application/x-www-form-urlencoded'
 
-        self.httpShouldReturn(self.makeMetaResponse(), scheme='http')
+        self.httpShouldReturn(self.metaResponseAsJson(), scheme='http')
 
         site = mwclient.Site('test.wikipedia.org')
 
@@ -145,7 +149,7 @@ class TestClient(TestCase):
     def test_force_https(self):
         # Setting https should work
 
-        self.httpShouldReturn(self.makeMetaResponse(), scheme='https')
+        self.httpShouldReturn(self.metaResponseAsJson(), scheme='https')
 
         site = mwclient.Site(('https', 'test.wikipedia.org'))
 
@@ -155,7 +159,7 @@ class TestClient(TestCase):
     def test_user_agent_is_sent(self):
         # User specified user agent should be sent sent to server
 
-        self.httpShouldReturn(self.makeMetaResponse())
+        self.httpShouldReturn(self.metaResponseAsJson())
 
         site = mwclient.Site('test.wikipedia.org', clients_useragent='MyFabulousClient')
 
@@ -164,7 +168,7 @@ class TestClient(TestCase):
     @responses.activate
     def test_basic_request(self):
 
-        self.httpShouldReturn(self.makeMetaResponse())
+        self.httpShouldReturn(self.metaResponseAsJson())
 
         site = mwclient.Site('test.wikipedia.org')
 
@@ -174,7 +178,7 @@ class TestClient(TestCase):
     @responses.activate
     def test_httpauth_defaults_to_basic_auth(self):
 
-        self.httpShouldReturn(self.makeMetaResponse())
+        self.httpShouldReturn(self.metaResponseAsJson())
 
         site = mwclient.Site('test.wikipedia.org', httpauth=('me', 'verysecret'))
 
@@ -183,7 +187,7 @@ class TestClient(TestCase):
     @responses.activate
     def test_httpauth_raise_error_on_invalid_type(self):
 
-        self.httpShouldReturn(self.makeMetaResponse())
+        self.httpShouldReturn(self.metaResponseAsJson())
 
         with pytest.raises(RuntimeError):
             site = mwclient.Site('test.wikipedia.org', httpauth=1)
@@ -201,7 +205,7 @@ class TestClient(TestCase):
     def test_version(self):
         # Should parse the MediaWiki version number correctly
 
-        self.httpShouldReturn(self.makeMetaResponse(version='1.16'))
+        self.httpShouldReturn(self.metaResponseAsJson(version='1.16'))
 
         site = mwclient.Site('test.wikipedia.org')
 
@@ -212,7 +216,7 @@ class TestClient(TestCase):
     def test_min_version(self):
         # Should raise MediaWikiVersionError if API version is < 1.16
 
-        self.httpShouldReturn(self.makeMetaResponse(version='1.15'))
+        self.httpShouldReturn(self.metaResponseAsJson(version='1.15'))
 
         with pytest.raises(mwclient.errors.MediaWikiVersionError):
             site = mwclient.Site('test.wikipedia.org')
@@ -231,6 +235,45 @@ class TestClient(TestCase):
         assert len(responses.calls) == 1
 
 
+class TestClientApiMethods(TestCase):
+
+    def setUp(self):
+        self.api = mock.patch('mwclient.client.Site.api').start()
+        self.api.return_value = self.metaResponse()
+        self.site = mwclient.Site('test.wikipedia.org')
+
+    def tearDown(self):
+        mock.patch.stopall()
+
+    def test_revisions(self):
+
+        self.api.return_value = {
+            'query': {'pages': {'1': {
+                'pageid': 1,
+                'title': 'Test page',
+                'revisions': [{
+                    'revid': 689697696,
+                    'timestamp': '2015-11-08T21:52:46Z',
+                    'comment': 'Test comment 1'
+                }, {
+                    'revid': 689816909,
+                    'timestamp': '2015-11-09T16:09:28Z',
+                    'comment': 'Test comment 2'
+                }]
+            }}}}
+
+        revisions = [rev for rev in self.site.revisions([689697696, 689816909], prop='content')]
+
+        args, kwargs = self.api.call_args
+        assert kwargs.get('revids') == '689697696|689816909'
+        assert len(revisions) == 2
+        assert revisions[0]['pageid'] == 1
+        assert revisions[0]['pagetitle'] == 'Test page'
+        assert revisions[0]['revid'] == 689697696
+        assert revisions[0]['timestamp'] == time.strptime('2015-11-08T21:52:46Z', '%Y-%m-%dT%H:%M:%SZ')
+        assert revisions[1]['revid'] == 689816909
+
+
 class TestClientUploadArgs(TestCase):
 
     def setUp(self):
@@ -238,7 +281,7 @@ class TestClientUploadArgs(TestCase):
 
     def configure(self, rights=['read', 'upload']):
 
-        self.raw_call.side_effect = [self.makeMetaResponse(rights=rights)]
+        self.raw_call.side_effect = [self.metaResponseAsJson(rights=rights)]
         self.site = mwclient.Site('test.wikipedia.org')
 
         self.vars = {
@@ -318,7 +361,7 @@ class TestClientGetTokens(TestCase):
         self.raw_call = mock.patch('mwclient.client.Site.raw_call').start()
 
     def configure(self, version='1.24'):
-        self.raw_call.return_value = self.makeMetaResponse(version=version)
+        self.raw_call.return_value = self.metaResponseAsJson(version=version)
         self.site = mwclient.Site('test.wikipedia.org')
         responses.reset()
 

commit bc8cf58237f929d8531d6d4f9f62eed09dd82848

Fix warning and crash in Site.revisions (#389)
This should fix the deprecation warning reported in #389, though
I am so far unable to reproduce it. The fix follows the same
pattern as the one for Page.revisions.

This also fixes mwclient throwing a KeyError if you call
Site.revisions with a props argument that does not include
timestamp. This was broken by c23abae , which didn't guard
against 'timestamp' not being in the response.

Signed-off-by: Adam Williamson <awilliam@redhat.com>

mwclient/client.py
@@ -1709,6 +1709,7 @@ class Site:
         self,
         revids: List[Union[int, str]],
         prop: str = 'ids|timestamp|flags|comment|user',
+        slots: str = 'main',
     ) -> List[Dict[str, Any]]:
         """Get data about a list of revisions.
 
@@ -1733,6 +1734,9 @@ class Site:
             'rvprop': prop,
             'revids': '|'.join(map(str, revids))
         }
+        if self.version[:2] > (1, 31):  # type: ignore[index]
+            # https://github.com/mwclient/mwclient/issues/389
+            kwargs['rvslots'] = slots
 
         revisions = []
         pages = self.get('query', **kwargs).get('query', {}).get('pages', {}).values()
@@ -1740,7 +1744,8 @@ class Site:
             for revision in page.get('revisions', ()):
                 revision['pageid'] = page.get('pageid')
                 revision['pagetitle'] = page.get('title')
-                revision['timestamp'] = parse_timestamp(revision['timestamp'])
+                if 'timestamp' in revision:
+                    revision['timestamp'] = parse_timestamp(revision['timestamp'])
                 revisions.append(revision)
         return revisions
 

test/integration.py
@@ -1,4 +1,5 @@
 import http.client
+import logging
 import shutil
 import subprocess
 import time
@@ -162,7 +163,7 @@ class TestLogin:
         assert pg.exists == False
 
 
-class TestClientLogin:
+class TestClient:
     def test_clientlogin_wrong_password(self, site):
         """Test we raise correct error for clientlogin() with wrong password."""
         with pytest.raises(mwclient.errors.LoginError):
@@ -180,6 +181,19 @@ class TestClientLogin:
         pg.edit("Hi I'm a new page", "create new page")
         pg = site.pages["Anonymous New Page"]
         assert pg.text() == "Hi I'm a new page"
+
+    def test_site_revisions(self, site, caplog):
+        caplog.set_level(logging.WARNING)
+        # as per docstring example, make sure we don't crash for lack
+        # of timestamp here
+        rev = site.revisions([1], prop="content")[0]
+        assert rev["pagetitle"] == "Main Page"
+        # without limiting response
+        rev = site.revisions([1])[0]
+        assert rev["pagetitle"] == "Main Page"
+        # check timestamp parsing
+        assert isinstance(rev["timestamp"], time.struct_time)
+        assert not caplog.text
 
 
 class TestUploadAsync:

test/test_client.py
@@ -724,7 +724,6 @@ class TestClientApiMethods(TestCase):
                     'comment': 'Test comment 1'
                 }, {
                     'revid': 689816909,
-                    'timestamp': '2015-11-09T16:09:28Z',
                     'comment': 'Test comment 2'
                 }]
             }}}}